ddae958a4be87eb13a09e1e62b1646e91bc2ea20
Sergey Selivanov <sergejdog04@gmail.com>
2024-12-13 19:39:38 +0300

    Initial Commit

---
+++ Homework_1/Homework_1/Homework_1.cpp
@@ -0,0 +1,48 @@
+﻿#include <locale.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class address {
+private:
+    string city;
+    string street;
+    int numberHouse;
+    int numberFlat;
+public:
+    address(string city, string street, int numHouse, int numFlat) {
+        this->city = city;
+        this->street = street;
+        this->numberHouse = numHouse;
+        this->numberFlat = numFlat;
+    }
+    address() : city(""), street(""), numberHouse(0), numberFlat(0) {}
+
+    string outputAddress() {
+        return city + " " + street + " " + to_string(numberHouse) + " " + to_string(numberFlat);
+    }
+};
+int main() {
+    setlocale(LC_ALL, "Rus");
+
+    ifstream date("in.txt");
+    ofstream outDate("out.txt");
+    int n;
+    date >> n;
+    string city, street;
+    int numHouse, numFlat;
+    address* listAddreses = new address[n];
+
+    for (int i = 0; i < n; i++) {
+        date >> city;
+        date >> street;
+        date >> numHouse;
+        date >> numFlat;
+        listAddreses[i] = address(city, street, numHouse, numFlat);
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        outDate << listAddreses[i].outputAddress() << endl;
+    }
+}
---
+++ Homework_2/Homework_2/Homework_2.cpp
@@ -0,0 +1,62 @@
+﻿#include <fstream>
+#include <string>
+
+using namespace std;
+
+class address {
+private:
+    string city;
+    string street;
+    int numberHouse;
+    int numberFlat;
+public:
+    address(string city, string street, int numHouse, int numFlat) {
+        this->city = city;
+        this->street = street;
+        this->numberHouse = numHouse;
+        this->numberFlat = numFlat;
+    }
+    address() : city(""), street(""), numberHouse(0), numberFlat(0) {}
+
+    
+
+    string outputAddress() {
+        return city + " " + street + " " + to_string(numberHouse) + " " + to_string(numberFlat);
+    }
+};
+
+void sortedAddress(address* addresses, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1; j++) {
+            address prRes = addresses[j];
+            if(addresses[j].outputAddress() > addresses[j + 1].outputAddress()){
+                addresses[j] = addresses[j + 1];
+                addresses[j + 1] = prRes;
+            }
+        }
+    }
+}
+
+int main() {
+
+    ifstream date("in.txt");
+    ofstream outDate("out.txt");
+    int n;
+    date >> n;
+    string city, street;
+    int numHouse, numFlat;
+    address* listAddreses = new address[n];
+
+    for (int i = 0; i < n; i++) {
+        date >> city;
+        date >> street;
+        date >> numHouse;
+        date >> numFlat;
+        listAddreses[i] = address(city, street, numHouse, numFlat);
+    }
+    sortedAddress(listAddreses, n);
+    for (int i = 0; i < n; i++) {
+        outDate << listAddreses[i].outputAddress() << endl;
+    }
+    return 0;
+}
